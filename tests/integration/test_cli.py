719ae4991035d46bc94af8399682b57dec6431e6
Sherzod K <sherzod.karimov@datadoghq.com>
2021-06-10 14:31:18 -0400

    use markers to run integration tests

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 
-@pytest.mark.skipif(os.getenv("DD_INTEGRATION") is None, reason="Set DD_INTEGRATION to run integration tests")
+@pytest.mark.integrations
 def test_cli(tmpdir, script_runner):
     with tmpdir.as_cwd():
         # Import
@@ -21,7 +21,7 @@ def test_cli(tmpdir, script_runner):
         assert ret.success
 
 
-@pytest.mark.skipif(os.getenv("DD_INTEGRATION") is None, reason="Set DD_INTEGRATION to run integration tests")
+@pytest.mark.integrations
 def test_cli_diff(tmpdir, script_runner):
     with tmpdir.as_cwd():
         # Import
