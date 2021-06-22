3193901768f20d4498f48372d18a70d5c1652a31
Sherzod K <sherzod.karimov@datadoghq.com>
2021-06-22 15:03:11 -0400

    add tests

---
+++ tests/unit/test_filters.py
@@ -0,0 +1,30 @@
+import pytest
+
+from datadog_sync.utils.filter import Filter
+
+
+@pytest.mark.parametrize(
+    "_filter, r_type, r_obj, expected",
+    [
+        (["Type=r_test;Name=attr;Value=exists;Operator:SubString"], "r_test", {"attr": "attr exists"}, True),
+        (["Type=r_test;Name=attr;Value=exists"], "r_test", {"test": "attr Exists"}, False),
+        (["Type=r_test_two;Name=attr;Value=exists"], "r_test", {"test": "attr exists"}, True),
+    ],
+)
+def test_filter(_filter, r_type, r_obj, expected):
+    filter_instance = Filter(_filter)
+
+    assert filter_instance.is_applicable(r_type, r_obj) == expected
+
+
+@pytest.mark.parametrize(
+    "_filter",
+    [
+        (["Type=r_test;Name=attr;Value=exists;Operator"]),
+        (["Type=;Name=attr;Value=exists;Operator"]),
+        (["Type="]),
+    ],
+)
+def test_invalid_filter(caplog, _filter):
+    Filter(_filter)
+    assert "invalid filter" in caplog.text
