6d840ba323d71b9c8c916374438d7716c71f10e8
NouemanKHAL <noueman.khal@gmail.com>
2021-07-01 14:53:38 +0200

    refactor to parametrized tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -37,13 +37,16 @@ def config():
     source_client = CustomClient(source_api_url, source_auth, retry_timeout)
     destination_client = CustomClient(destination_api_url, destination_auth, retry_timeout)
 
-    cfg = Configuration(logger=logging.getLogger(__name__), source_client=source_client, destination_client=destination_client)
+    cfg = Configuration(
+        logger=logging.getLogger(__name__), source_client=source_client, destination_client=destination_client
+    )
 
-    resources = [
-        cls(cfg) for cls in models.__dict__.values() if isinstance(cls, type) and issubclass(cls, BaseResource)
-    ]
+    resources = {
+        cls.resource_type: cls(cfg)
+        for cls in models.__dict__.values()
+        if isinstance(cls, type) and issubclass(cls, BaseResource)
+    }
 
     cfg.resources = resources
 
     return cfg
-
--- tests/integration/test_import.py
+++ tests/integration/test_import.py
@@ -1,9 +1,30 @@
 import pytest
+import os
+
+from datadog_sync import constants
+from datadog_sync import models
+from datadog_sync.utils.base_resource import BaseResource
+
+
+all_resources = [
+    cls.resource_type for cls in models.__dict__.values() if isinstance(cls, type) and issubclass(cls, BaseResource)
+]
 
 
 @pytest.mark.vcr
 @pytest.mark.integration
-def test_import(config):
-    for resource in config.resources:
-        resource.import_resources()
-        assert resource.source_resources
+@pytest.mark.parametrize("resource_type", [resource_type for resource_type in all_resources])
+def test_import_resources(config, resource_type):
+    resource = config.resources[resource_type]
+    resource.import_resources()
+    assert resource.source_resources
+
+
+@pytest.mark.vcr
+@pytest.mark.integration
+@pytest.mark.parametrize("resource_type", [resource_type for resource_type in all_resources])
+def test_sync_resources(config, resource_type):
+    resource = config.resources[resource_type]
+    resource.import_resources()
+    resource.apply_resources()
+    assert not resource.check_diffs()
