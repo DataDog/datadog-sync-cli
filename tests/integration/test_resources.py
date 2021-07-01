6cd11cc46db8a0472a4ac0b950d057d1c367c73a
NouemanKHAL <noueman.khal@gmail.com>
2021-07-01 17:36:41 +0200

    Update tests

--- tests/integration/test_resources.py
+++ tests/integration/test_resources.py
@@ -1,14 +1,23 @@
 import pytest
 import os
+import copy
 
 from datadog_sync import constants
 from datadog_sync import models
 from datadog_sync.utils.base_resource import BaseResource
+from datadog_sync.cli import get_import_order
 
 
-all_resources = [
-    cls.resource_type for cls in models.__dict__.values() if isinstance(cls, type) and issubclass(cls, BaseResource)
-]
+# TODO: Fix order of sync
+
+str_to_class = dict(
+        (cls.resource_type, cls)
+        for cls in models.__dict__.values()
+        if isinstance(cls, type) and issubclass(cls, BaseResource)
+    )
+
+
+all_resources = get_import_order([cls for cls in models.__dict__.values() if isinstance(cls, type) and issubclass(cls, BaseResource)], str_to_class)
 
 
 @pytest.mark.vcr
@@ -16,8 +25,14 @@ all_resources = [
 @pytest.mark.parametrize("resource_type", [resource_type for resource_type in all_resources])
 def test_import_resources(config, resource_type):
     resource = config.resources[resource_type]
+
     resource.import_resources()
     assert resource.source_resources
+
+    original = copy.deepcopy(resource.source_resources)
+    resource.import_resources()
+
+    assert resource.source_resources == original
 
 
 @pytest.mark.vcr
@@ -25,6 +40,14 @@ def test_import_resources(config, resource_type):
 @pytest.mark.parametrize("resource_type", [resource_type for resource_type in all_resources])
 def test_sync_resources(config, resource_type):
     resource = config.resources[resource_type]
+
     resource.import_resources()
     resource.apply_resources()
+
     assert not resource.check_diffs()
+
+    original = copy.deepcopy(resource.destination_resources)
+    resource.apply_resources()
+
+    assert resource.destination_resources == original
+
