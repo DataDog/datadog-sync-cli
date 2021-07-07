8ab03527a2deda71c7c795260a6f05a25badb45b
NouemanKHAL <noueman.khal@gmail.com>
2021-07-07 16:39:48 +0200

    Ignore unsupported resources

--- datadog_sync/cli.py
+++ datadog_sync/cli.py
@@ -140,7 +140,7 @@ def get_resources(cfg, resources_arg, force_missing_deps):
         if isinstance(cls, type) and issubclass(cls, BaseResource)
     )
 
-    resources_classes = [str_to_class[resource_type] for resource_type in resources_arg]
+    resources_classes = [str_to_class[resource_type] for resource_type in resources_arg if resource_type in str_to_class]
 
 
     order_list = get_import_order(resources_classes, str_to_class)
