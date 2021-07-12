7c0b5ed2044cf06e92d46fc5a81db8c9d19f64b7
Sherzod K <sherzod.karimov@datadoghq.com>
2021-07-12 11:46:07 -0400

    update exceptions and hadle special  cases

--- datadog_sync/utils/resource_utils.py
+++ datadog_sync/utils/resource_utils.py
@@ -2,7 +2,13 @@ import re
 
 
 class ResourceConnectionError(Exception):
-    pass
+    def __init__(self, resource_type, _id=None):
+        self.resource_type = resource_type
+        self._id = _id
+
+        super(ResourceConnectionError, self).__init__(
+            "Failed to connect resource. Import and sync resource: {} with ID: {}".format(resource_type, _id)
+        )
 
 
 def replace(keys_list, origin, r_obj, resource_to_connect, connection_resources_obj):
@@ -16,6 +22,9 @@ def replace(keys_list, origin, r_obj, resource_to_connect, connection_resources_
         keys_list = keys_list.split(".", 1)
 
         if len(keys_list) == 1 and keys_list[0] in r_obj:
+            # Handle cases where value is null/None
+            if not r_obj[keys_list[0]]:
+                return
             replace_ids(keys_list[0], origin, r_obj, resource_to_connect, connection_resources_obj)
             return
 
@@ -33,12 +42,12 @@ def replace_ids(key, origin, r_obj, resource_to_connect, connection_resources_ob
                     new_id = f"{connection_resources_obj[resource_to_connect][_id]['id']}"
                     r_obj[key] = re.sub(_id + r"([^#]|$)", new_id + "# ", r_obj[key])
                 else:
-                    raise ResourceConnectionError(
-                        f"missing resource connection. Import {resource_to_connect} with ID: {_id} from source"
-                    )
+                    raise ResourceConnectionError(resource_to_connect, _id)
 
             r_obj[key] = (r_obj[key].replace("#", "")).strip()
 
+            return
+        if resource_to_connect == "monitors" and key == "query":
             return
 
         if origin == "service_level_objectives" and resource_to_connect == "synthetics_tests":
@@ -50,9 +59,7 @@ def replace_ids(key, origin, r_obj, resource_to_connect, connection_resources_ob
                         found = True
                         r_obj[key][i] = val["monitor_id"]
                 if not found:
-                    raise ResourceConnectionError(
-                        f"missing resource connection. Import {resource_to_connect} with ID: {_id} from source"
-                    )
+                    raise ResourceConnectionError(resource_to_connect, _id)
             return
 
         if isinstance(r_obj[key], list):
@@ -63,18 +70,14 @@ def replace_ids(key, origin, r_obj, resource_to_connect, connection_resources_ob
                     if _id in connection_resources_obj[resource_to_connect]:
                         r_obj[key][i] = connection_resources_obj[resource_to_connect][_id]["id"]
                     else:
-                        raise ResourceConnectionError(
-                            f"missing resource connection. Import {resource_to_connect} with ID: {_id} from source"
-                        )
+                        raise ResourceConnectionError(resource_to_connect, _id)
             else:
                 for i in range(len(r_obj[key])):
                     _id = r_obj[key][i]
                     if _id in connection_resources_obj[resource_to_connect]:
                         r_obj[key][i] = f"{connection_resources_obj[resource_to_connect][_id]['id']}"
                     else:
-                        raise ResourceConnectionError(
-                            f"missing resource connection. Import {resource_to_connect} with ID: {_id} from source"
-                        )
+                        raise ResourceConnectionError(resource_to_connect, _id)
         else:
             if origin == "downtimes":
                 _id = str(r_obj[key])
@@ -83,9 +86,7 @@ def replace_ids(key, origin, r_obj, resource_to_connect, connection_resources_ob
                 elif not r_obj[key]:
                     return
                 else:
-                    raise ResourceConnectionError(
-                        f"missing resource connection. Import {resource_to_connect} with ID: {_id} from source"
-                    )
+                    raise ResourceConnectionError(resource_to_connect, _id)
             else:
                 if r_obj[key] in connection_resources_obj[resource_to_connect]:
                     if resource_to_connect == "synthetics_tests":
@@ -93,8 +94,6 @@ def replace_ids(key, origin, r_obj, resource_to_connect, connection_resources_ob
                     else:
                         r_obj[key] = f"{connection_resources_obj[resource_to_connect][r_obj[key]]['id']}"
                 else:
-                    raise ResourceConnectionError(
-                        f"missing resource connection. Import {resource_to_connect} with ID: {r_obj[key]} from source"
-                    )
+                    raise ResourceConnectionError(resource_to_connect, r_obj[key])
     else:
-        raise ResourceConnectionError(f"missing resource connection. Import {resource_to_connect} from source")
+        raise ResourceConnectionError(resource_to_connect)
