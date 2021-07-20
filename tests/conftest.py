40419f38835d3dee06afa5e3f92037091b91d616
NouemanKHAL <noueman.khal@gmail.com>
2021-07-20 17:59:12 +0200

    cleanup config fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -50,29 +50,10 @@ def vcr_config():
 
 @pytest.fixture(scope="module")
 def config():
-    source_api_url = os.getenv(constants.DD_SOURCE_API_URL)
-    destination_api_url = os.getenv(constants.DD_DESTINATION_API_URL)
     max_workers = os.getenv(constants.MAX_WORKERS)
-
-    # Initialize the datadog API Clients
-    source_auth = {
-        "apiKeyAuth": os.getenv(constants.DD_SOURCE_API_KEY),
-        "appKeyAuth": os.getenv(constants.DD_SOURCE_APP_KEY),
-    }
-    destination_auth = {
-        "apiKeyAuth": os.getenv(constants.DD_DESTINATION_API_KEY),
-        "appKeyAuth": os.getenv(constants.DD_DESTINATION_APP_KEY),
-    }
-
-    retry_timeout = os.getenv(constants.DD_HTTP_CLIENT_RETRY_TIMEOUT, 60)
-
-    source_client = CustomClient(source_api_url, source_auth, retry_timeout)
-    destination_client = CustomClient(destination_api_url, destination_auth, retry_timeout)
 
     cfg = Configuration(
         logger=logging.getLogger(__name__),
-        source_client=source_client,
-        destination_client=destination_client,
         max_workers=int(max_workers),
     )
 
