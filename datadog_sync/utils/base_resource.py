245d8b7b284bc87625cd3c10e2c390652c913f94
NouemanKHAL <noueman.khal@gmail.com>
2021-07-20 17:32:31 +0200

    fix thread_pool_executor

--- datadog_sync/utils/base_resource.py
+++ datadog_sync/utils/base_resource.py
@@ -6,9 +6,8 @@ from pprint import pformat
 from deepdiff import DeepDiff
 
 from datadog_sync.constants import RESOURCE_FILE_PATH
-from datadog_sync.utils.resource_utils import replace
+from datadog_sync.utils.resource_utils import replace, thread_pool_executor
 
-from concurrent.futures import ThreadPoolExecutor
 
 
 class BaseResource:
@@ -32,7 +31,7 @@ class BaseResource:
         pass
 
     def import_resources_concurrently(self, resources):
-        with ThreadPoolExecutor(self.config.max_workers) as executor:
+        with thread_pool_executor(self.config.max_workers) as executor:
             futures = [executor.submit(self.process_resource_import, resource) for resource in resources]
             for future in futures:
                 try:
@@ -119,7 +118,7 @@ class BaseResource:
         resources = kwargs.get("resources")
         if resources == None:
             resources = self.source_resources
-        with ThreadPoolExecutor(self.config.max_workers) as executor:
+        with thread_pool_executor(self.config.max_workers) as executor:
             futures = [
                 executor.submit(
                     self.prepare_resource_and_apply,
