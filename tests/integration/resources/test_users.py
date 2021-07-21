d05124f2952024f2665b9e723efe5dfb03a5d23a
NouemanKHAL <noueman.khal@gmail.com>
2021-07-21 10:56:02 +0200

    allow update on users and roles

--- tests/integration/resources/test_users.py
+++ tests/integration/resources/test_users.py
@@ -7,8 +7,3 @@ from datadog_sync.models import Users
 class TestUsersResources(BaseResourcesTestClass):
     resource_type = Users.resource_type
     field_to_update = "attributes.name"
-
-    # FIXME: reintroduce after nested attribute update support
-    @pytest.mark.skip(reason="nested attribute update is not currently supported in tests")
-    def test_resource_update_sync(self):
-        pass
