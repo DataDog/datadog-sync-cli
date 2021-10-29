a98166aae7b303f6ca3b6181b34961454f3dd0d3
Sherzod K <sherzod.karimov@datadoghq.com>
2021-10-29 13:19:51 -0400

    dont skip metric tag config tests

--- tests/integration/resources/test_metric_tag_configurations.py
+++ tests/integration/resources/test_metric_tag_configurations.py
@@ -2,13 +2,11 @@
 # under the 3-clause BSD style license (see LICENSE).
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2019 Datadog, Inc.
-import pytest
 
 from datadog_sync.models import MetricTagConfigurations
 from tests.integration.helpers import BaseResourcesTestClass
 
 
-@pytest.mark.skip
 class TestMetricConfigurationResources(BaseResourcesTestClass):
     resource_type = MetricTagConfigurations.resource_type
     field_to_update = "attributes.tags."
