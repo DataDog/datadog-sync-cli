e272853ed3fb359398d2a01a17c1820336106572
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2023-03-17 16:30:09 -0400

    black

--- scripts/build.py
+++ scripts/build.py
@@ -5,21 +5,19 @@ import PyInstaller.__main__
 
 from datadog_sync._version import __version__
 
-PACKAGE_NAME="datadog-sync-cli"
-ENTRY_POINT="datadog_sync/cli.py"
-DEFAULT_NAME=f"{PACKAGE_NAME}-{platform.system()}-{platform.machine()}"
-OUTPUT_DIR="dist"
+PACKAGE_NAME = "datadog-sync-cli"
+ENTRY_POINT = "datadog_sync/cli.py"
+DEFAULT_NAME = f"{PACKAGE_NAME}-{platform.system()}-{platform.machine()}"
+OUTPUT_DIR = "dist"
 
-parser = argparse.ArgumentParser(
-    description="Build binary"
-)
+parser = argparse.ArgumentParser(description="Build binary")
 parser.add_argument("--onefile", "-f", action="store_true", help="Create a one-file bundled executable.")
 parser.add_argument("--file-name", "-n", help="App name.", default=DEFAULT_NAME)
 parser.add_argument("--output", "-o", help="Binary output directory.", default=OUTPUT_DIR)
 args = parser.parse_args()
 
 # Build args
-cmd_args=[ENTRY_POINT]
+cmd_args = [ENTRY_POINT]
 cmd_args.extend(["--name", args.file_name])
 cmd_args.extend(["--distpath", args.output])
 if args.onefile:
