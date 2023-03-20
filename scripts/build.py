2718295a119e7cd6767bd71eadc3951f7b03d67a
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2023-03-20 10:53:53 -0400

    add and fix build script

--- scripts/build.py
+++ scripts/build.py
@@ -1,19 +1,25 @@
+import os
 import platform
 import argparse
 
 import PyInstaller.__main__
+from github import Github
 
 from datadog_sync._version import __version__
 
+REPO = "datadog/datadog-sync-cli"
 PACKAGE_NAME = "datadog-sync-cli"
 ENTRY_POINT = "datadog_sync/cli.py"
-DEFAULT_NAME = f"{PACKAGE_NAME}-{platform.system()}-{platform.machine()}"
+DEFAULT_NAME = f"{PACKAGE_NAME}-{platform.system().lower()}-{platform.machine().lower()}"
 OUTPUT_DIR = "dist"
+GITHUB_ASSET_UPLOAD = ""
 
 parser = argparse.ArgumentParser(description="Build binary")
 parser.add_argument("--onefile", "-f", action="store_true", help="Create a one-file bundled executable.")
 parser.add_argument("--file-name", "-n", help="App name.", default=DEFAULT_NAME)
 parser.add_argument("--output", "-o", help="Binary output directory.", default=OUTPUT_DIR)
+parser.add_argument("--upload", "-u", action="store_true", help="Upload artifact to latest release")
+
 args = parser.parse_args()
 
 # Build args
@@ -26,3 +32,13 @@ if args.onefile:
 PyInstaller.__main__.run(
     cmd_args,
 )
+
+if args.upload:
+    token = os.environ.get("GITHUB_TOKEN")
+    if not token:
+        raise Exception("github token is required to upload")
+    g = Github(token)
+    repo = g.get_repo(REPO)
+    latest_release = repo.get_latest_release()
+
+    latest_release.upload_asset(path=f"{OUTPUT_DIR}/{DEFAULT_NAME}")
