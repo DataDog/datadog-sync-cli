938076ba660492cce3b5ef1d99bc41a60da1729d
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2023-07-10 11:53:30 -0400

    remove inspect import

--- datadog_sync/model/monitors.py
+++ datadog_sync/model/monitors.py
@@ -6,7 +6,6 @@
 from __future__ import annotations
 import re
 from typing import TYPE_CHECKING, Optional, List, Dict, cast
-import inspect
 
 from datadog_sync.utils.base_resource import BaseResource, ResourceConfig
 
