618511750c5af9763c79ef7d6126f8bf50d7b8cb
aldrick.castro <aldrick.castro@datadoghq.com>
2023-12-22 14:56:26 -0500

    Final testing and recording new cassettes

--- datadog_sync/model/incidents.py
+++ datadog_sync/model/incidents.py
@@ -25,16 +25,18 @@ class Incidents(BaseResource):
         excluded_attributes=[
             "id",
             "attributes.public_id",
-            "attributes.commander",  # somehow returned by create or update, not by get
-            "attributes.last_modified_by",  # somehow returned by create or update, not by get
+            "attributes.commander",  
+            "attributes.last_modified_by",  
             "attributes.last_modified_by_uuid",
             "attributes.created",
             "attributes.modified",
-            "attributes.created_by",  # somehow returned by create or update, not by get
+            "attributes.created_by",  
             "attributes.created_by_uuid",
-            "attributes.notification_handles",  # too hard to support properly, also, it gives wrong dates, and possibly spams people, we don't want that; ok to loose that info
+            "attributes.notification_handles",  
             "attributes.time_to_resolve",
-            "attributes.customer_impact_duration",  # computed field
+            "attributes.customer_impact_duration",  
+            "attributes.time_to_internal_response",
+            "attributes.resolved",
             "relationships.created_by_user",
             "relationships.last_modified_by_user",
             "relationships.user_defined_fields",
@@ -47,21 +49,20 @@ class Incidents(BaseResource):
             "attributes.creation_idempotency_key",
             "attributes.customer_impact_scope",
         ],
-
     )
     # Additional Incidents specific attributes
     pagination_config = PaginationConfig(
         page_size=100,
         page_number_param="page[offset]",
         page_size_param="page[size]",
-        # this endpoint uses offset (number of items) instead of page number, workaround the paginated client by reusing `page_number` to store offset instead (computed here because we don't have `resp`)
+        # this endpoint uses offset (number of items) instead of page number, workaround the paginated client by reusing
+        # `page_number` to store offset instead (computed here because we don't have `resp`)
         page_number_func=lambda idx, page_size, page_number: page_size * (idx + 1),
         # just return 1, the pagination loop already handles breaking when a page is smaller than page size
         remaining_func=lambda *args: 1,
     )
 
     def get_resources(self, client: CustomClient) -> List[Dict]:
-        # we return the incidents in public_id order, so creating them on a fresh organizations will gives us the same public_id in source & destination organizations
 
         resp = client.paginated_request(client.get)(
             self.resource_config.base_path, pagination_config=self.pagination_config
@@ -81,8 +82,15 @@ class Incidents(BaseResource):
             resource["attributes"]["visibility"] = "organization"
 
         # let's do some deepomatic-specific incidents fields migrations:
-        if "Namespace" in resource["attributes"]["fields"] and resource["attributes"]["fields"]["Namespace"]["value"] is not None and "kube_namespace" in resource["attributes"]["fields"] and resource["attributes"]["fields"]["kube_namespace"]["value"] is None:
-            resource["attributes"]["fields"]["kube_namespace"]["value"] = resource["attributes"]["fields"]["Namespace"]["value"]
+        if (
+            "Namespace" in resource["attributes"]["fields"]
+            and resource["attributes"]["fields"]["Namespace"]["value"] is not None
+            and "kube_namespace" in resource["attributes"]["fields"]
+            and resource["attributes"]["fields"]["kube_namespace"]["value"] is None
+        ):
+            resource["attributes"]["fields"]["kube_namespace"]["value"] = resource["attributes"]["fields"]["Namespace"][
+                "value"
+            ]
             resource["attributes"]["fields"]["Namespace"]["value"] = None
 
         self.resource_config.source_resources[resource["id"]] = resource
@@ -96,7 +104,6 @@ class Incidents(BaseResource):
     def create_resource(self, _id: str, resource: Dict) -> None:
         destination_client = self.config.destination_client
         payload = {"data": resource}
-        # the datadog api documentation says only a subset of accepted fields for creation; in practice it does handles only a subset, and ignores the others
         resp = destination_client.post(
             self.resource_config.base_path,
             payload,
@@ -104,7 +111,8 @@ class Incidents(BaseResource):
 
         self.resource_config.destination_resources[_id] = resp["data"]
 
-        # create doesn't accept everything right away, e.g. attributes.resolved; follow the create by an update to sync more data
+        # create doesn't accept everything right away, e.g. attributes.resolved; 
+        # follow the create by an update to sync more data
         self.update_resource(_id, resource)
 
     def update_resource(self, _id: str, resource: Dict) -> None:
@@ -112,8 +120,7 @@ class Incidents(BaseResource):
         payload = {"data": resource}
 
         resp = destination_client.patch(
-            self.resource_config.base_path +
-            f"/{self.resource_config.destination_resources[_id]['id']}",
+            self.resource_config.base_path + f"/{self.resource_config.destination_resources[_id]['id']}",
             payload,
         ).json()
 
@@ -122,8 +129,7 @@ class Incidents(BaseResource):
     def delete_resource(self, _id: str) -> None:
         destination_client = self.config.destination_client
         destination_client.delete(
-            self.resource_config.base_path +
-            f"/{self.resource_config.destination_resources[_id]['id']}"
+            self.resource_config.base_path + f"/{self.resource_config.destination_resources[_id]['id']}"
         )
 
     def connect_id(self, key: str, r_obj: Dict, resource_to_connect: str) -> Optional[List[str]]:
