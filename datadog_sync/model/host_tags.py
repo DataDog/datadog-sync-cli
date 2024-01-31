1e58c568c5f05a17b863161185b119a787856429
skarimo <40482491+skarimo@users.noreply.github.com>
2024-01-31 17:03:48 -0500

    Handle host remapping in get call (#206)
    * handle host remmaping in get call
    
    * remove unused import
    
    * pin pytest

--- datadog_sync/model/host_tags.py
+++ datadog_sync/model/host_tags.py
@@ -4,7 +4,8 @@
 # Copyright 2019 Datadog, Inc.
 
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional, List, Dict, cast
+from collections import defaultdict
+from typing import TYPE_CHECKING, Optional, List, Dict
 
 from datadog_sync.utils.base_resource import BaseResource, ResourceConfig
 
@@ -22,19 +23,21 @@ class HostTags(BaseResource):
     def get_resources(self, client: CustomClient) -> List[Dict]:
         resp = client.get(self.resource_config.base_path).json()
 
-        return list(resp["tags"].items())
+        import_hosts = defaultdict(list)
+        for tag, hosts in resp["tags"].items():
+            for host in hosts:
+                import_hosts[host].append(tag)
+
+        return [{k: v} for k, v in import_hosts.items()]
 
     def import_resource(self, _id: Optional[str] = None, resource: Optional[Dict] = None) -> None:
         if _id:
             return  # This should never occur. No resource depends on it.
 
-        resource = cast(dict, resource)
-        tag = resource[0]
-        hosts = resource[1]
-        for host in hosts:
-            if host not in self.resource_config.source_resources:
-                self.resource_config.source_resources[host] = []
-            self.resource_config.source_resources[host].append(tag)
+        host = list(resource.keys())[0]
+        tags = list(resource.values())[0]
+
+        self.resource_config.source_resources[host] = tags
 
     def pre_resource_action_hook(self, _id, resource: Dict) -> None:
         pass
