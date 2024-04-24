f8386dbdfb6a209d2c324d3c7811f1b69275f34d
skarimo <40482491+skarimo@users.noreply.github.com>
2024-04-24 10:48:51 -0400

    Ensure we dump synced resources before exiting when interupted (#232)
    * cleanup and clean-exit

--- datadog_sync/commands/diffs.py
+++ datadog_sync/commands/diffs.py
@@ -2,7 +2,6 @@
 # under the 3-clause BSD style license (see LICENSE).
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2019 Datadog, Inc.
-import asyncio
 
 from click import command
 
@@ -12,8 +11,8 @@ from datadog_sync.commands.shared.options import (
     destination_auth_options,
     non_import_common_options,
 )
-from datadog_sync.utils.resources_handler import run_cmd_async
 from datadog_sync.constants import Command
+from datadog_sync.commands.shared.utils import run_cmd
 
 
 @command(Command.DIFFS.value, short_help="Log resource diffs.")
@@ -23,4 +22,4 @@ from datadog_sync.constants import Command
 @non_import_common_options
 def diffs(**kwargs):
     """Log Datadog resources diffs."""
-    asyncio.run(run_cmd_async(Command.DIFFS, **kwargs))
+    run_cmd(Command.DIFFS, **kwargs)
---
+++ datadog_sync/commands/shared/utils.py
@@ -0,0 +1,58 @@
+import asyncio
+import os
+from sys import exit
+
+from datadog_sync.constants import DESTINATION_ORIGIN, DESTINATION_RESOURCES_DIR, SOURCE_RESOURCES_DIR, Command
+from datadog_sync.utils.configuration import Configuration, build_config
+from datadog_sync.utils.resource_utils import dump_resources
+from datadog_sync.utils.resources_handler import ResourcesHandler
+
+
+def run_cmd(cmd: Command, **kwargs):
+    # Build config
+    cfg = build_config(cmd, **kwargs)
+
+    # Initiate resources handler
+    handler = ResourcesHandler(cfg)
+
+    try:
+        asyncio.run(run_cmd_async(cfg, handler, cmd))
+    except KeyboardInterrupt:
+        cfg.logger.error("Process interrupted by user")
+        if cmd == Command.SYNC:
+            cfg.logger.info("Writing synced resources to disk before exit...")
+            synced_resource_types = set(handler.resources_manager.all_resources_to_type.values())
+            dump_resources(cfg, synced_resource_types, DESTINATION_ORIGIN)
+            exit(0)
+
+    if cfg.logger.exception_logged:
+        exit(1)
+
+
+async def run_cmd_async(cfg: Configuration, handler: ResourcesHandler, cmd: Command):
+    try:
+        # Initiate async items
+        await cfg.init_async(cmd)
+        await handler.init_async()
+
+        cfg.logger.info(f"Starting {cmd.value}...")
+
+        # Run specific handler
+        if cmd == Command.IMPORT:
+            os.makedirs(SOURCE_RESOURCES_DIR, exist_ok=True)
+            await handler.import_resources()
+        elif cmd == Command.SYNC:
+            os.makedirs(DESTINATION_RESOURCES_DIR, exist_ok=True)
+            await handler.apply_resources()
+        elif cmd == Command.DIFFS:
+            await handler.diffs()
+        else:
+            cfg.logger.error(f"Command {cmd.value} not found")
+            return
+
+        cfg.logger.info(f"Finished {cmd.value}")
+    finally:
+        await cfg.exit_async()
+        # Disable progress bar so it doesn't interfere with the logger
+        if handler.worker.pbar:
+            handler.worker.pbar.disable = True
