9c34ed5d57e3fdca8a97ad47a8e359dffd374da4
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2024-07-17 10:48:09 -0400

    add lic

--- datadog_sync/utils/storage/_base_storage.py
+++ datadog_sync/utils/storage/_base_storage.py
@@ -1,3 +1,8 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the 3-clause BSD style license (see LICENSE).
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019 Datadog, Inc.
+
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from typing import Any, Dict
