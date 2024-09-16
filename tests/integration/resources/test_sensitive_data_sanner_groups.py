e10f7087e5f1647377cb5a2761d158a748f41523
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2024-09-16 15:11:14 -0400

    unskip groups tests

--- tests/integration/resources/test_sensitive_data_sanner_groups.py
+++ tests/integration/resources/test_sensitive_data_sanner_groups.py
@@ -2,14 +2,11 @@
 # under the 3-clause BSD style license (see LICENSE).
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2019 Datadog, Inc.
-import pytest
 
-from tests.conftest import get_record_mode
 from tests.integration.helpers import BaseResourcesTestClass
 from datadog_sync.models import SensitiveDataScannerGroups
 
 
-@pytest.mark.skipif(get_record_mode() != "new_episodes", reason="test is run in integration mode only")
 class TestSensitiveDataScannerGroupsResources(BaseResourcesTestClass):
     resource_type = SensitiveDataScannerGroups.resource_type
     field_to_update = "attributes.name"
