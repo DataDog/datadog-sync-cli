eea354187f28d1b72eea53b9b6b220df04f4296f
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2024-09-16 14:51:06 -0400

    add support for sds group order

---
+++ tests/integration/resources/test_sensitive_data_scanner_groups_order.py
@@ -0,0 +1,17 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the 3-clause BSD style license (see LICENSE).
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019 Datadog, Inc.
+
+import pytest
+
+from tests.integration.helpers import BaseResourcesTestClass
+from datadog_sync.models import SensitiveDataScannerGroupsOrder, SensitiveDataScannerGroups
+
+
+class TestSensitiveDataScannerGroupsOrder(BaseResourcesTestClass):
+    resource_type = f"{SensitiveDataScannerGroupsOrder.resource_type},{SensitiveDataScannerGroups.resource_type}"
+
+    @pytest.mark.skip(reason="resource is only updated by default")
+    def test_resource_update_sync(self):
+        pass
