4fcc6823daa8899e9f311437ea3f483fe5fb6573
michael.richey <michael.richey@datadoghq.com>
2025-04-07 13:13:45 -0400

    Fix import

--- datadog_sync/commands/_import.py
+++ datadog_sync/commands/_import.py
@@ -6,6 +6,7 @@
 from click import command
 
 from datadog_sync.commands.shared.options import (
+    common_options,
     destination_auth_options,
     source_auth_options,
     storage_options,
@@ -19,7 +20,6 @@ from datadog_sync.constants import Command
 @destination_auth_options
 @common_options
 @storage_options
-@common_options
 def _import(**kwargs):
     """Import Datadog resources."""
     run_cmd(Command.IMPORT, **kwargs)
